c60e82f6c8d142b8d0cedcf3cd1cb906aa7447ba
danny417 <danny417@interchange.ubc.ca>
2013-01-20 03:20:51 -0800

    .

---
+++ software/TowerDefence/Game.h
@@ -0,0 +1,25 @@
+/*
+ * Game.h
+ *
+ *  Created on: 2013-01-20
+ *      Author: Danny
+ */
+
+#ifndef GAME_H_
+#define GAME_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "sys/alt_alarm.h"
+#include "animation.h"
+#include "Object.h"
+#include <altera_up_sd_card_avalon_interface.h>
+#include "hardCode.h"
+#include "Env.h"
+
+
+alt_up_pixel_buffer_dma_dev* pixel_buffer;
+alt_up_char_buffer_dev* char_buffer;
+
+#endif /* GAME_H_ */
