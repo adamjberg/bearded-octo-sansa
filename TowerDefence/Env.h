d7298b79ec9e0f54c4ea5573968dd083073b5b5b
danny417 <danny417@interchange.ubc.ca>
2013-01-19 19:40:37 -0800

    Working Project Folder

---
+++ TowerDefence/Env.h
@@ -0,0 +1,21 @@
+/*
+ * Env.h
+ *
+ *  Created on: 2013-01-19
+ *      Author: Danny
+ */
+
+#ifndef ENV_H_
+#define ENV_H_
+
+#include "Object.h"
+
+struct Env {
+	struct Object** o;
+	int size;
+	int coord[2];
+};
+
+struct Env* initEnv();
+void addToEnv(struct Env*, struct Object*);
+#endif /* ENV_H_ */
---
+++ TowerDefence/Object.h
@@ -0,0 +1,26 @@
+/*
+ * Object.h
+ *
+ *  Created on: 2013-01-17
+ *      Author: Danny
+ */
+
+#ifndef OBJECT_H_
+#define OBJECT_H_
+#include "animation.h"
+#include <stdlib.h>
+
+struct Object {
+	int x, y, scale, prev_x, prev_y, center_x, center_y, total_stats;
+	struct animation *currImg;
+	struct animation* stats[3];
+	void (*animate)(alt_up_pixel_buffer_dma_dev*, struct animation**, int, int, int, int);
+	void (*collide)(struct Object*, struct Object*);
+};
+
+struct Object* initObject(int, int, int, struct animation*);
+void setXY(struct Object*, int, int);
+void killObject(struct Object*);
+void setStats(struct Object*, int);
+void addStats(struct Object*, struct animation*);
+#endif /* OBJECT_H_ */
